e66b830c02256f7a59853cbc67fde5da99934fe2
zhuzhenjie <mimotronik@gmail.com>
2022-01-02 07:53:52 +0800

    feat: lock

--- src/main/java/org/iproute/lock/MyLock.java
+++ src/main/java/org/iproute/lock/MyLock.java
@@ -7,16 +7,30 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class MyLock {
 
-    static ReentrantLock lock = new ReentrantLock();
+    static ReentrantLock lock = new ReentrantLock(true);
 
     public static void main(String[] args) {
+        new Thread(() -> {
+            sayHello();
+        }, "t1").start();
 
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new Thread(() -> {
+            sayHello();
+        }, "t2").start();
     }
 
     static void sayHello() {
         lock.lock();
         try {
             System.out.println(Thread.currentThread().getName());
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             lock.unlock();
         }
