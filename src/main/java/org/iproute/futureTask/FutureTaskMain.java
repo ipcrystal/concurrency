d389a1a4384ace18a0f3d9725e700fad7c723337
zhuzhenjie <mimotronik@gmail.com>
2023-05-05 14:55:13 +0800

    ft

---
+++ src/main/java/org/iproute/futureTask/FutureTaskMain.java
@@ -0,0 +1,28 @@
+package org.iproute.futureTask;
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * FutureTaskMain
+ *
+ * @author zhuzhenjie
+ * @since 5/5/2023
+ */
+public class FutureTaskMain {
+
+    public static void main(String[] args) throws Exception {
+
+        FutureTask<String> futureTask = new FutureTask<>(
+                () -> {
+                    Thread.sleep(3_000);
+                    System.out.println("hello world");
+                    return "hello world";
+                }
+        );
+        new Thread(futureTask).start();
+
+        System.out.println(futureTask.get(1, TimeUnit.SECONDS));
+
+    }
+}
