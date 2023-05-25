9bbe246de213d97df8f81a5bd094c59f9a50f37c
zhuzhenjie <mimotronik@gmail.com>
2023-05-25 14:12:43 +0800

    ft: jdk thread join

---
+++ jdk/src/main/java/org/iproute/join/JoinMain.java
@@ -0,0 +1,37 @@
+package org.iproute.join;
+
+/**
+ * JoinMain
+ *
+ * @author zhuzhenjie
+ * @since 5/25/2023
+ */
+public class JoinMain {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Runnable a = () -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                threadMsg(i);
+            }
+        };
+
+        Thread thread = new Thread(a, "A");
+
+        thread.start();
+        thread.join(2_000);
+
+        threadMsg("this is main thread");
+    }
+
+    static void threadMsg(Object msg) {
+        String threadName = Thread.currentThread().getName();
+        System.out.println("[" + threadName + "] msg = " + msg);
+    }
+
+}
